9d60d25aa8f1eb9772733081e837cd3efc673297
Sepahrad Salour <sepahrad.salour@gmail.com>
2019-01-17 12:41:07 +0330

    1- Adding config directory to the project 2- Enabling usb devices when program starts 3- Changing version 0.9 to 1.0

--- src/main.py
+++ src/main.py
@@ -5,6 +5,14 @@ import subprocess
 import os
 import sys
 
+try:
+    CREATE_NO_WINDOW = 0x08000000
+    subprocess.run(['assets/exe/devcon.exe', 'enable', 'usb*'], shell=False, creationflags=CREATE_NO_WINDOW, text=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
+    logger.log.info("All devices enabled at main when program started.")
+except:
+    logger.log.critical("Could not execute devcon at main.")
+    sys.exit(1)
+
 if __name__ == "__main__":
 
     if serialmanager.SerialExists():
@@ -18,7 +26,7 @@ if __name__ == "__main__":
 try:
     CREATE_NO_WINDOW = 0x08000000
     subprocess.run(['assets/exe/devcon.exe', 'enable', 'usb*'], shell=False, creationflags=CREATE_NO_WINDOW, text=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
-    logger.log.info("All devices enabled at main.")
+    logger.log.info("All devices enabled at main when program killed.")
 except:
     logger.log.critical("Could not execute devcon at main.")
     sys.exit(1)
